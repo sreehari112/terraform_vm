27853fa13a4c7684a232149a2c860c247c064232
sreehari112 <sreeharireddy11@gmail.com>
2020-11-20 20:03:18 +0530

    Add files

---
+++ var.tf
@@ -0,0 +1,11 @@
+
+variable "key_name" {default = "hariterraform"}
+
+variable "ami" {default = "ami-00ddb0e5626798373"}
+variable "instance_type" {default = "t2.micro"}
+variable "region" { 
+default = "us-east-1"
+}
+variable "count" {
+default = "1"
+}
